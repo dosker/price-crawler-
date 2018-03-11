c29686da6317eab0e86daec5986205728ad443a2
dosker@develop <dosker@develop>
2018-03-11 15:08:04 +0800

    sidekiq setup done

---
+++ honestbee_crawler/app/jobs/test_job.rb
@@ -0,0 +1,9 @@
+class TestJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+    Rails.logger.debug "#{self.class.name}: I'm performing my job with arguments: #{args.inspect}"
+    
+  end
+end
